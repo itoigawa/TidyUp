f8b1379c09a779536d658850a5357cc67ab15a88
itoigawa <707788koo@gmail.com>
2020-10-27 16:37:07 +0900

    delete config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,6 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
-  config.assets.css_compressor = :sass
-  config.public_file_server.enabled = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
